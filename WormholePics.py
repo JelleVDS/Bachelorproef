1c0d26bd11ebe38e4c91e2e0a6e41d689d4a209d
JelleVDS <81178738+JelleVDS@users.noreply.github.com>
2021-10-30 22:29:06 +0200

    yog
    puig

---
+++ WormholePics.py
@@ -0,0 +1,143 @@
+# import WormholeRayTracer as wrmhl
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+# Inladen foto's
+img_saturn    = cv2.imread('Saturn.jpg')
+img_gargantua = cv2.imread('wormhole.jpg')
+
+# print(len(img_saturn))
+
+#Maak lijsten om dichtste te zoeken
+vertical   = len(img_saturn)     #1024
+horizontal = len(img_saturn[0])  #2048
+
+theta_list = list()
+for teller in range(0, 1024):
+    theta = (np.pi/vertical) * teller - np.pi
+    theta_list.append(theta)
+
+phi_list =list()
+for teller in range(0, 2048):
+    phi   = (2*np.pi/horizontal) * teller + np.pi
+    # Nulpunt in het midden van het scherm zetten:
+    if phi > 2*np.pi:
+        phi = phi - 2*np.pi
+    phi_list.append(phi)
+
+def photo_to_sphere(photo):
+    """
+    Give the pixels of the pictures a spherical coordinate
+        -herschalen
+        -reduceren naar Ny x Nz
+
+    Input:  - photo: de pixels van de photo in sferische coordinaten
+    Output: - dict: een dictionary met als sleutel (theta, phi) en als waarde
+              de RGB-value van de bijbehorende pixel
+    """
+
+    dict = {}
+    vertical   = len(photo)     #1024
+    horizontal = len(photo[0])  #2048
+    for row in range(0, vertical):
+        for column in range(0, horizontal):
+            theta = (np.pi/vertical) * row - np.pi
+            phi   = (2*np.pi/horizontal) * column + np.pi
+            # Nulpunt in het midden van het scherm zetten:
+            if phi > 2*np.pi:
+                phi = phi - 2*np.pi
+
+            coordinate = (theta, phi) #Tuple with angles that will be used as key
+            pixel      = np.array([photo[row][column]]) #RGB-values
+            dict[coordinate] = pixel
+
+    return dict
+
+
+def decide_universe(photo, saturn, gargantua):
+    """
+    Decides whether ray is in Saturn or Gargantua universe and accesses the
+    according function to determine the RGB values of the pixels.
+
+    Input:  - photo:     solved ray tracer
+            - saturn: spherical picture of the Saturn side
+            - gargantua: spherical picture of the other side
+    Output: - picture:   Matrix with RGB values for cv2
+    """
+    picture = []
+    for rij in range(len(photo[0])):
+        row = []
+        for kolom in range(len(photo[0][0])):
+            if photo[0][rij,kolom] <= 0:
+                pixel = ray_to_gar((photo[1][rij][kolom], photo[2][rij][kolom]), gargantua)
+            else:
+                pixel = ray_to_saturn((photo[1][rij][kolom], photo[2][rij][kolom]), saturn)
+
+            row.append(np.array(pixel))
+
+        picture.append(np.array(row))
+    img = cv2.cvtColor(np.array(picture, np.float32), 1)
+    return img
+
+def distance(x, position):
+    """
+    Define a distance function for closest neighbour
+    """
+    dist = abs(x-position)
+
+    return dist
+
+
+def ray_to_saturn(position, saturn):
+    """
+    Determines values of the pixels for the rays at the Saturn side.
+
+    Input:  - position: tuple of theta and phi angles: [theta, phi]
+            - saturn: spherical picture of the Saturn side
+    Output: - List with RBG-values of corresponding pixel of the Saturn picture
+    """
+    t, p = position
+    screen = saturn.keys()
+    theta_near = min(theta_list, key=lambda x: distance(x, t))
+    phi_near = min(phi_list, key=lambda x: distance(x, p))
+    nearest = (theta_near, phi_near)
+    print(nearest)
+    RGB = saturn.get(nearest)
+
+    return RGB
+
+def ray_to_gar(position, gargantua):
+    """
+    Determines values of the pixels for the rays at the Gargantua side.
+
+    Input:  - position: tuple of theta and phi angles: [theta, phi]
+            - gargantua: spherical picture of the other side
+    Output: - List with RBG-values of corresponding pixel of the Saturn picture
+    """
+    t, p = position
+    screen = gargantua.keys()
+    theta_near = min(theta_list, key=lambda x: distance(x, t))
+    phi_near = min(phi_list, key=lambda x: distance(x, p))
+    nearest = (theta_near, phi_near)
+    RGB = gargantua.get(nearest)
+
+    return RGB
+
+
+saturn      = photo_to_sphere(img_saturn)
+np.savez('sat', saturn)
+gargantua   = photo_to_sphere(img_gargantua)
+np.savez('gar', gargantua)
+
+raytracer = np.load('ray_solved.npy')
+# print(raytracer)
+# saturn = np.load('sat.npz')
+# gargantua = np.load('gar.npz')
+pic = decide_universe(raytracer, saturn, gargantua)
+np.savez('picInterstellar', pic)
+# print(pic)
+
+path = os.getcwd()
+cv2.imwrite(os.path.join(path, 'InterstellarWormhole.png'), pic)
--- WormholeRayTracer.py
+++ WormholeRayTracer.py
@@ -190,11 +190,19 @@ def Make_Pict_RB(q):
 
     # Integration
     for i in range(N):
+<<<<<<< Updated upstream
         p, q , H_i = integrator(p, q, Cst, h)
         # Motion.append([p, q])
         # H.append(H_i)
     # H.append(DNeg_Ham(p, q))
 
+=======
+        p, q , CM_i = integrator(p, q, Cst, h)
+        # Motion.append([p, q])
+        # CM.append(CM_i)
+    # CM.append(DNeg_CM(p, q))
+    Motion = q
+>>>>>>> Stashed changes
     end = time.time()
 
     print(end - start)
@@ -202,10 +210,106 @@ def Make_Pict_RB(q):
     pict = Make_Pict_RB(q, q1, 200, 0.5, 0.2)
     #print(pict)
 
+<<<<<<< Updated upstream
     return np.array(Motion), pict , np.array(H)
 
 
 def Make_Pict_RB(q, q0, N_a, R, w):
+=======
+    return np.array(Motion), pict , np.array(CM)
+
+def diff_equations(t, variables):
+    l, theta, phi, p_l, p_th, p_phi = variables
+    r = dneg_r(l)
+    rec_r = 1/r
+    rec_r_2 = rec_r**2
+    rec_r_3 = rec_r_2*rec_r
+    sin1 = np.sin(theta)
+    cos1 = np.cos(theta)
+    sin2 = sin1**2
+    sin3 = sin1*sin2
+    H1 = p_l**2
+    H2 = p_th**2*rec_r_2
+    H3 = p_phi**2/sin2*rec_r_2
+    # H = 0.5*sum_subd((H1 + H2 + H3))
+    B = p_th**2 + p_phi**2/sin2
+    b = p_phi
+
+    #Using the hamiltonian equations of motion
+    dl_dt       = p_l
+    dtheta_dt   = p_th * rec_r_2
+    dphi_dt     = b / sin2 * rec_r_2
+    dpl_dt      = B**2 * (dneg_dr_dl(l)) * rec_r_3
+    dpth_dt     = b ** 2 * cos1 / sin3 * rec_r_2
+
+    diffeq = [dl_dt, dphi_dt, dtheta_dt, dpl_dt, np.zeros(dl_dt.shape), dpth_dt]
+    return diffeq
+
+
+def simulate_raytracer(h, N, q0, Nz = 14**2, Ny = 14**2, methode = 'RK45'):
+    """
+    Solves the differential equations using a build in solver (solve_ivp) with
+    specified method.
+    Input:  - methode: method used for solving the ivp (standerd runge-kutta of fourth order)
+            - h: stepsize
+            - N: number of steps
+            - q0: initial position of the camera
+            - Nz: number of vertical pixels
+            - Ny: number of horizontal pixels
+
+    Output: - sol.y: a matrix with each row containing the initial and end value
+              for one ray: [l, phi, theta, p_l, p_phi, p_theta]
+    """
+    S_c = screen_cart(Nz, Ny)
+    S_cT = np.transpose(S_c, (2,0,1))
+    S_sph = cart_Sph(S_cT)
+    p, Cst = inn_momenta(S_c, S_sph, Cst_DNeg, inn_mom_DNeg)
+    p1, p2, p3 = p
+    q1, q2, q3 = q0
+    t_end = N*h
+    endpos = []
+    endmom = []
+
+    for teller1 in range(0, len(p1)):
+        row_pos = []
+        row_mom = []
+        for teller2 in range(0, len(p1[0])):
+            initial_values = np.array([q1, q3, q2, p1[teller1][teller2], p2[teller1][teller2], p3[teller1][teller2]])
+            sol         = integr.solve_ivp(diff_equations, [0, t_end], initial_values, method = methode, t_eval=[0, t_end])
+            #Reads out the data from the solution
+            l_end       = sol.y[0][-1]
+            phi_end     = sol.y[1][-1]
+            theta_end   = sol.y[2][-1]
+            pl_end      = sol.y[3][-1]
+            pphi_end    = sol.y[4][-1]
+            ptheta_end  = sol.y[5][-1]
+            # adds local solution to row
+            row_pos.append([l_end, phi_end, theta_end])
+            row_mom.append([pl_end, pphi_end, ptheta_end])
+        # adds row to matrix
+        endpos.append(row_pos)
+        endmom.append(row_mom)
+    return endpos, endmom
+
+
+def Make_Pict_RB(q):
+    # input: q: matrix with coordinates in configuration space on first row ouput:
+    # 3D matrix (2D matrix of rays each containing a coordinate in colorspace)
+    # RGB based on sign(l), q = (l, phi, theta)
+    pict = []
+    for j in range(len(q[0])):
+        row = []
+        for i in range(len(q[0][0])):
+            if q[0][j,i] <= 0:
+                row.append([255, 0, 0])
+            else:
+                row.append([0, 0, 255])
+        pict.append(row)
+    return cv2.cvtColor(np.array(pict, np.float32), 1)
+
+
+def Make_Pict_RGBP(q, q0, N_a, R, w):
+>>>>>>> Stashed changes
     # input: q: matrix with coordinates in configuration space on first row
     #        q0: starting position (unused)
     #        3D matrix (2D matrix of rays each containing a coordinate in colorspace)
@@ -369,17 +473,33 @@ def gdsc(Motion):
 
 
 #initial position in spherical coord
+<<<<<<< Updated upstream
 # Motion1, Photo1, H1 = Simulate_DNeg(Smpl.Sympl_DNeg, 0.01, 1500, np.array([5, 3, 2]), 20**2, 20**2)
 Motion2, Photo2, H2 = Simulate_DNeg(rk.runge_kutta, 0.01, 5000, np.array([5, 3, 2]), 20**3, 20**3)
 
 # plot_Ham(H1)
 #plot_Ham(H2)
+=======
+# Motion1, Photo1, CM1 = Simulate_DNeg(Smpl.Sympl_DNeg, 0.01, 1500, np.array([5, 3, 2]), 20**2, 20**2)
+# Motion2, Photo2, CM2 = Simulate_DNeg(rk.runge_kutta, 0.01, 1000, 9, 20**2, 20**2)
+# np.save('ray_solved', Motion1)
+# plot_CM(CM1, ['H', 'b', 'B**2'])
+# plot_CM(CM2, ['H', 'b', 'B**2'])
+>>>>>>> Stashed changes
 
 # gdsc(Motion1)
 #gdsc(Motion2)
 
+<<<<<<< Updated upstream
 path = os.getcwd()
 # print(path)
 # cv2.imwrite(path + '/DNeg_Sympl.png', 255*Photo1)
 cv2.imwrite(path + '/DNeg Kutta.png', 255*Photo2)
+>>>>>>> Stashed changes
+=======
+# path = os.getcwd()
+# cv2.imwrite(os.path.join(path, 'DNeg Sympl.png'), 255*Photo1)
+# cv2.imwrite(path + '/DNeg Kutta.png', 255*Photo2)
+
+print(simulate_raytracer(0.01, 200, [5, 3, 3], methode = 'RK45'))
 >>>>>>> Stashed changes
