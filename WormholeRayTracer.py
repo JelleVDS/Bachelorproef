Update partially fixed rotation, updated new pictures, now with grid too

WormholeRayTracer.py
@@ -289,8 +289,8 @@ def rotate_ray(ray, Nz, Ny):
             y = int(width + Ny/2)
             l, phi, theta = ray[r]
 
-            phi_rot     = height
-            theta_rot   = width
+            phi_rot     = width*(2*np.pi/Nz)
+            theta_rot   = height*(np.pi/Ny)
 
             phi = phi + phi_rot
             theta = theta + theta_rot
@@ -581,7 +581,7 @@ if __name__ == '__main__':
     Nz = 200
     Ny = 400
     start = time.time()
-    sol = simulate_raytracer(20, [17.5, np.pi, np.pi/2], Nz, Ny, methode = 'RK45')
+    sol = simulate_raytracer(22, [20, np.pi, np.pi/2], Nz, Ny, methode = 'RK45')
     end = time.time()
     print('Tijdsduur = ' + str(end-start))
     momenta, position = sol
@@ -591,6 +591,11 @@ if __name__ == '__main__':
     print('saving location...')
     np.save('raytracer2', picture)
     print('location saved!')
+
+    print('Saving picture')
+    path = os.getcwd()
+    cv2.imwrite(os.path.join(path, 'picture2.png'), picture)
+    print('Picture saved')
 
     # print(picture)
     # if mode ==  0: