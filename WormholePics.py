9542985f8ac5f781ecf419e00f65fa61b6a4c7de
JelleVDS <81178738+JelleVDS@users.noreply.github.com>
2021-11-12 19:18:10 +0100

    Revert "Update WormholePics.py"
    This reverts commit 66726225f85990c73510eaf1568d3be1d61f39cd, reversing
    changes made to 36d7aca2388d006d8e0880a2f1a526c414a8c78e.

--- WormholePics.py
+++ WormholePics.py
@@ -3,10 +3,6 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-from numba import jit
-from numba.core import types
-from numba.typed import Dict
-from numba import njit
 
 # Inladen foto's
 print('Reading in pictures...')
@@ -33,9 +29,6 @@ for teller in range(0, horizontal):
     #     phi = phi - 2*np.pi
     phi_list.append(phi)
 # print('here2')
-
-key_ty = types.Tuple((types.float64, types.float64))
-# @njit
 def photo_to_sphere(photo):
     """
     Give the pixels of the pictures a spherical coordinate
@@ -44,7 +37,7 @@ def photo_to_sphere(photo):
               de RGB-value van de bijbehorende pixel
     """
 
-    dict = Dict.empty(key_type = key_ty, value_type = types.float64[:],)
+    dict = {}
     vertical   = len(photo)     #1024
     horizontal = len(photo[0])  #2048
     for row in range(0, vertical):
@@ -52,13 +45,24 @@ def photo_to_sphere(photo):
             theta = (np.pi/vertical) * row #- np.pi
             phi   = (2*np.pi/horizontal) * column #+ np.pi
             # Nulpunt in het midden van het scherm zetten:
-            # coordinate = types.Tuple(theta, phi) #Tuple with angles that will be used as key
+            coordinate = (theta, phi) #Tuple with angles that will be used as key
             pixel      = np.array([photo[row][column]]) #RGB-values
-            dict[(theta, phi)] = pixel
+            dict[coordinate] = pixel
 
     return dict
 
-@jit
+
+# ph = photo_to_sphere(img_saturn)
+# im = np.array([])
+# for element in ph:
+#     print(element)
+#     theta, phi = element
+#     im[theta][phi] = ph[element]
+#
+# path = os.getcwd()
+# cv2.imwrite(os.path.join(path, 'test.png'), im)
+
+
 def decide_universe(photo, saturn, gargantua):
     """
     Decides whether ray is in Saturn or Gargantua universe and accesses the
@@ -73,23 +77,10 @@ def decide_universe(photo, saturn, gargantua):
         row = []
         for kolom in range(len(photo[0])):
             if photo[rij][kolom][0] < 0:
-                # pixel = ray_to_rgb((photo[rij][kolom][1], photo[rij][kolom][2]), gargantua)
-                ph = photo[rij][kolom][1]
-                th = photo[rij][kolom][2]
-
-                theta_near = min(theta_list, key=lambda x: distance(x, th))
-                phi_near = min(phi_list, key=lambda x: distance(x, ph))
-                nearest = (theta_near, phi_near)
-                pixel = gargantua[nearest]
+                pixel = ray_to_rgb((photo[rij][kolom][1], photo[rij][kolom][2]), gargantua)
             else:
-                # pixel = ray_to_rgb((photo[rij][kolom][1], photo[rij][kolom][2]), saturn)
-                ph = photo[rij][kolom][1]
-                th = photo[rij][kolom][2]
+                pixel = ray_to_rgb((photo[rij][kolom][1], photo[rij][kolom][2]), saturn)
 
-                theta_near = min(theta_list, key=lambda x: distance(x, th))
-                phi_near = min(phi_list, key=lambda x: distance(x, ph))
-                nearest = (theta_near, phi_near)
-                pixel = gargantua[nearest]
             [[R, G, B]] = pixel
             row.append([R, G, B])
         picture.append(np.array(row))
@@ -123,22 +114,70 @@ def ray_to_rgb(position, saturn):
 
     return RGB
 
+# saturn      = photo_to_sphere(img_saturn)
+# print('Saturn image loaded.')
+# gargantua   = photo_to_sphere(img_gargantua)
+# print('Gargantua image loaded.')
+# # import numpy as np
+#
+# raytracer = wrmhole.wormhole_with_symmetry(steps=3000, initialcond = [70, np.pi, np.pi/2], Nz=200, Ny=400, Par=[0.43/1.42953, 8.6, 43])
+# # print(raytracer.shape)
+# print('Ray tracer solution loaded.')
+# print('Starting image placing process...')
+# pic = decide_universe(raytracer, saturn, gargantua)
+# # print(pic)
+# print('Image placing completed.')
+# print('Saving picture')
+# path = os.getcwd()
+# cv2.imwrite(os.path.join(path, 'param_70_0.43_8.6_43.png'), pic)
+# print('Picture saved')
 
-if __name__ == '__main__':
-    saturn      = photo_to_sphere(img_saturn)
-    print('Saturn image loaded.')
-    gargantua   = photo_to_sphere(img_gargantua)
-    print('Gargantua image loaded.')
+# l = [53.79, 58.05, 96.75]
+# W = [0.43, 0.12, 0.55]
+# a = [0.043, 4.3, 43]
 
-    raytracer = wrmhole.wormhole_with_symmetry(tijd=22, initialcond = [20, np.pi, np.pi/2], Nz=200, Ny=400, Par=[0.43/1.42953, 1, 0])
-    # print(raytracer.shape)
-    print('Ray tracer solution loaded.')
-    print('Starting image placing process...')
-    # @jit(nopython=True)
-    pic = decide_universe(raytracer, saturn, gargantua)
-    print(pic)
-    print('Image placing completed.')
-    print('Saving picture')
-    path = os.getcwd()
-    cv2.imwrite(os.path.join(path, 'Pictures_test_x=0.5.png'), pic)
-    print('Picture saved')
+# for i in range(0, len(l)):
+#     print('started with l:'+str(i))
+#     raytracer_l = wrmhole.wormhole_with_symmetry(steps=3000, initialcond = [l[i], np.pi, np.pi/2], Nz=200, Ny=400, Par=[0.43/1.42953, 8.6, 4.3])
+#
+#     pic_l = decide_universe(raytracer_l, saturn, gargantua)
+#     path = os.getcwd()
+#     cv2.imwrite(os.path.join(path, 'param_'+str(l[i])+'_0.43_8.6_4.3.png'), pic_l)
+#     print('pictures saved:'+str(i))
+
+# for i in range(0, len(W)):
+#     print('started with W:'+str(i))
+#     raytracer_W = wrmhole.wormhole_with_symmetry(steps=3000, initialcond = [70, np.pi, np.pi/2], Nz=200, Ny=400, Par=[W[i]/1.42953, 8.6, 4.3])
+#     # raytracer_a = wrmhole.wormhole_with_symmetry(steps=3000, initialcond = [300, np.pi, np.pi/2], Nz=200, Ny=400, Par=[0.43/1.42953, 8.6, a[i]])
+#
+#     pic_W = decide_universe(raytracer_W, saturn, gargantua)
+#     path = os.getcwd()
+#     cv2.imwrite(os.path.join(path, 'param_70_'+str(W[i])+'_8.6_4.3.png'), pic_W)
+#     print('pictures saved:'+str(i))
+#
+# for i in range(0, len(a)):
+#     print('started with a:'+str(i))
+#     raytracer_a = wrmhole.wormhole_with_symmetry(steps=3000, initialcond = [70, np.pi, np.pi/2], Nz=200, Ny=400, Par=[0.43/1.42953, 8.6, a[i]])
+#
+#     pic_a = decide_universe(raytracer_a, saturn, gargantua)
+#     path = os.getcwd()
+#     cv2.imwrite(os.path.join(path, 'param_70_43_8.6_'+str(a[i])+'.png'), pic_a)
+#     print('pictures saved:'+str(i))
+
+# if __name__ == '__main__':
+#     saturn      = photo_to_sphere(img_saturn)
+#     print('Saturn image loaded.')
+#     gargantua   = photo_to_sphere(img_gargantua)
+#     print('Gargantua image loaded.')
+#
+#     raytracer = wrmhole.wormhole_with_symmetry(time=22, initialcond = [20, np.pi, np.pi/2], Nz=400, Ny=400, Par=[0.43/1.42953, 1, 0])
+#     # print(raytracer.shape)
+#     print('Ray tracer solution loaded.')
+#     print('Starting image placing process...')
+#     pic = decide_universe(raytracer, saturn, gargantua)
+#     print(pic)
+#     print('Image placing completed.')
+#     print('Saving picture')
+#     path = os.getcwd()
+#     cv2.imwrite(os.path.join(path, 'Pictures_test_x=0.5.png'), pic)
+#     print('Picture saved')
