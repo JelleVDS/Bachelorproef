fd153dec8932d872959fb4c889265e02d979e92f
Sasha <92683199+sash444@users.noreply.github.com>
2021-11-27 22:23:20 +0100

    symmetry with qubit method

--- sasha.py
+++ sasha.py
@@ -6,22 +6,14 @@ import numpy as np
 import InbeddingDiagramDNeg as emb
 import os
 
-img_saturn, img_gargantua, thetalist, philist = wrmpics.read_in_pictures('four400.png','negfour400.png')
 
-saturn      = wrmpics.photo_to_sphere(img_saturn)
-print('Saturn image loaded.')
-gargantua   = wrmpics.photo_to_sphere(img_gargantua)
-print('Gargantua image loaded.')
+gar = 'four.png'
+sat = 'negfour.png'
 
-raytracer = wrmhole.wormhole_with_symmetry(tijd=100, initialcond = [50, np.pi, np.pi/2], Nz=400, Ny=400, Par=[0.43/1.42953, 1, 0.43])
-# print(raytracer.shape)
-print('Ray tracer solution loaded.')
-print('Starting image placing process...')
-pic = wrmpics.decide_universe(raytracer, saturn, gargantua, thetalist, philist)
-print(pic)
-# print(pic)
-print('Image placing completed.')
+wormh = wrmhole.wormhole_with_symmetry(tijd = 2000,q0 = [96.75, np.pi, np.pi/2], Nz=1024, Ny=2048, Par=[0.43/1.42953, 8.6, 4.3])
+picture = wrmpics.make_pic_quick(wormh, sat, gar)
+np.save('picture', picture)
 print('Saving picture')
 path = os.getcwd()
-cv2.imwrite(os.path.join(path, 'CORRECTFOURCOLORS_t=100__L=50__W=0.43__rho=1__a=0.43.png'), pic)
+cv2.imwrite(os.path.join(path, 'testqubitswithgrad.png'), picture)
 print('Picture saved')
