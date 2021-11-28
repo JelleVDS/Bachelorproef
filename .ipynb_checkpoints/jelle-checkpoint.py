10f17664b2229b32627efe3ef38cabfa28382425
JelleVDS <81178738+JelleVDS@users.noreply.github.com>
2021-11-28 15:08:29 +0100

    small changes

--- .ipynb_checkpoints/jelle-checkpoint.py
+++ .ipynb_checkpoints/jelle-checkpoint.py
@@ -8,7 +8,7 @@ import time
 gar = 'wormhole.jpg'
 sat = 'Saturn.jpg'
 
-wormh = raytracer.wormhole_with_symmetry(t_end = 10000 ,q0 = [200, np.pi, np.pi/2], Nz=1000, Ny=2000, Par=[0.43/1.42953, 8.3, 0.43])
+wormh = raytracer.wormhole_with_symmetry2(t_end = 2000, Nz=1024, Ny=2048, q0 = [96.75, np.pi, np.pi/2], Par=[0.43/1.42953, 8.6, 0.43])
 start = time.time()
 picture = pics.make_pic_quick(wormh, sat, gar)
 np.save('picture', picture)
