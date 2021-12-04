7643014ae5f5d01ac4ac2a3f8ee44c98f6df4608
JelleVDS <81178738+JelleVDS@users.noreply.github.com>
2021-12-04 18:24:21 +0100

    fotos en parameters

--- jelle2.py
+++ jelle2.py
@@ -10,10 +10,9 @@ import os
 sat = 'negfourfull16thick.png'
 gar = 'fourfull.png'
 
-wormh = wrmhole.wormhole_with_symmetry(t_end = 150, q0 = [8.75, np.pi, np.pi/2], Nz=1024, Ny=2048, Par=[0.02/1.42953, 1, 2.5], h=10**-10, choice=True)
+wormh = wrmhole.wormhole_with_symmetry(t_end = 150, q0 = [6.95, np.pi, np.pi/2], Nz=1024, Ny=2048, Par=[0.014, 1, 0.9], h=10**-10, choice=True)
 picture = wrmpics.make_pic_quick(wormh, sat, gar)
-np.save('picture', picture)
-print('Saving picture')
+print('Saving picture...')
 path = os.getcwd()
 cv2.imwrite(os.path.join(path, 'testJelle4.png'), picture)
 print('Picture saved')
